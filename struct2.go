9c06ae960e3c227be5bec65a6ee51d3942ff3ef1
feivorid <feivorid@gmail.com>
2020-09-04 12:05:49 +0800

    新增类的成员方法

--- struct2.go
+++ struct2.go
@@ -13,14 +13,25 @@ func NewStudent(id int64, name string, male bool, score float64) *Student {
 	return &Student{id, name, male, score}
 }
 
+/**
+获取name
+*/
 func (s Student) GetName() string {
 	return s.Name
+}
+
+/**
+设置name
+*/
+func (s *Student) SetName(name string) {
+	s.Name = name
 }
 
 func main() {
 	student := NewStudent(1, "joe", true, 64.1)
 	fmt.Println(student)
 
+	student.SetName("joe1")
 	name := student.GetName()
 	fmt.Println(name)
 }
